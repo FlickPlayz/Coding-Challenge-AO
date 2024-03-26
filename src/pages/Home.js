5ddcd7c8700a3f2755cbe63bbb39410431e99505
Aitizaz Ansar <aitizaz.ansar@live.com>
2024-03-26 02:04:41 +0000

    fixed styling on home screen, just need to make the buttons actually send to relevant pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,32 +2,22 @@ import '../Layout.css';
 import '../StyleSheet/Home.css';
 import React from 'react';
 
-  /*
-   line 16  add modal-content and add btn within it
-    */
-
-
 const Home = () => {
   const showModal = true; 
   return (
     <div>
-    <div className= "container">
     <div className={`modal ${showModal ? 'show' : ''}`}>
-  
-    </div>
-    <div className='btns'>
-        <div className='btn'>
-        <button>Week 1</button>
+        <div>
+        <button className='btn'>Week 1</button>
         </div>
-        <div className='btn'>
-        <button>Week 2</button>
+        <div>
+        <button className='btn'>Week 2</button>
         </div>
-        <div className='btn'>
-        <button>Coming Soon!</button>
+        <div>
+        <button className='btn'>Coming Soon!</button>
         </div>
         </div>
-      </div>
-    </div>
+        </div>
   );
 }
 
